87348c69fd8e50abbf39afa1e3092e49893c0955
Russ Green <1886088+russgreen@users.noreply.github.com>
2025-08-24 09:57:12 +0100

    Add WPF application with logging and secret management
    - Updated `.gitignore` to exclude sensitive files.
    - Added `WpfTestApp` project to the solution.
    - Created `App.xaml` and `App.xaml.cs` for application startup.
    - Updated `AssemblyInfo.cs` for WPF theme information.
    - Introduced `Directory.Build.targets` for build-time secrets management.
    - Implemented `Host.cs` for application hosting and Serilog configuration.
    - Defined UI in `MainWindow.xaml` with logging buttons.
    - Handled logging in `MainWindow.xaml.cs` for button actions.
    - Created project file `WpfTestApp.csproj` with necessary dependencies.

---
+++ WpfTestApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Configuration;
+using System.Data;
+using System.Reflection;
+using System.Windows;
+
+namespace WpfTestApp;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private ILogger<App> _logger;
+
+    void App_Startup(object sender, StartupEventArgs e)
+    {
+        Host.StartHost().Wait();
+
+        _logger = Host.GetService<ILogger<App>>();
+        _logger.LogInformation("Application {version} started.", Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion);
+
+        MainWindow mainView = new();
+        mainView.Show();
+
+        return;
+    }
+}
+
---
+++ WpfTestApp/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Logging;
+using Serilog.Context;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTestApp;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private ILogger<MainWindow> _logger;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        _logger = Host.GetService<ILogger<MainWindow>>();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        using (LogContext.PushProperty("UsageTracking", true))
+        {
+            _logger.LogInformation("Logged information");
+        }
+
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        using (LogContext.PushProperty("UsageTracking", true))
+        {
+            _logger.LogWarning("Logged warning");
+        }
+
+    }
+
+    private void Button_Click_2(object sender, RoutedEventArgs e)
+    {
+        using (LogContext.PushProperty("UsageTracking", true))
+        {
+            _logger.LogError("Logged error");
+        }
+
+    }
+
+    private void Button_Click_3(object sender, RoutedEventArgs e)
+    {
+        using (LogContext.PushProperty("UsageTracking", true))
+        {
+            _logger.LogCritical("Logged critical");
+        }
+
+    }
+
+    private void Button_Click_4(object sender, RoutedEventArgs e)
+    {
+            throw new Exception(); 
+    }
+}
